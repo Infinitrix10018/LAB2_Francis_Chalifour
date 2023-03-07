ced40d1c02729372304cf6d1c554027bae50cb4d
fchaf <Infinitrix@outlook.fr>
2023-03-07 15:44:57 -0500

    Cours 7 mars

---
+++ Assets/_MyAsset/Scripts/FinDeJeu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinDeJeu : MonoBehaviour
+{
+
+    private int _pointage;
+
+    public bool _fin;
+
+    [SerializeField] private int _max_nbr = 10;
+
+    private float _tempsTotal;
+
+    private int _numeroScene = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (_fin)
+        //{
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        //}
+
+    }
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+
+                // récupére l'index de la scene
+                int noScene = SceneManager.GetActiveScene().buildIndex;
+                float _numeroScene = SceneManager.sceneCount;
+
+                //debug de test.
+                Debug.Log(--_numeroScene);
+
+                if (noScene == _numeroScene)
+                {
+                // message de fin basique
+                    Debug.Log("Bravo! Vous avez reussi! Vous avez pris " + Time.time + " secondes pour compléter la course et vous avez accrocher " + _pointage + " obstacle(s), pour un temps total de " + _tempsTotal + "secondes");
+
+
+                    Debug.Log("fin de partie");
+                }
+                else
+                {
+                    SceneManager.LoadScene(noScene + 1);
+                }
+
+            }
+
+        }
+    
+}
